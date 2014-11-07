64bf30fd553e9f6f532a7f1b122ceac4d668bce4
Steve Liu <steveliu.api@gmail.com>
2014-11-07 14:47:08 +0800

    testing

--- src/Simulator/CacheSimulation.java
+++ src/Simulator/CacheSimulation.java
@@ -6,7 +6,6 @@ import Object.Bus;
 import Protocol.MESI;
 import Protocol.MSI;
 
-import java.util.*;
 import java.io.*;
 
 public class CacheSimulation {
@@ -29,8 +28,9 @@ public class CacheSimulation {
     }
 
     public void run(String inputFile, String numProcessor) throws Exception{
-        String path = "/Trace logs/"+inputFile.toLowerCase()+numProcessor;
+        String path = "Trace logs/"+inputFile.toLowerCase()+numProcessor;
         File dir = new File(path);
+
         if(!dir.isDirectory()) {
             System.out.println("no such folder");
             System.exit(0);
@@ -38,15 +38,23 @@ public class CacheSimulation {
         File[] files = dir.listFiles();
         BufferedReader[] readers = new BufferedReader[files.length];
         for (int i = 0; i < files.length; i++) {
+            System.out.println(files[i].getName());
             readers[i] = new BufferedReader(new FileReader(files[i]));
+            System.out.println("reader "+i+"  "+readers[i].toString());
         }
         String line;
+        int counter = 0;
         while((line = readers[0].readLine()) != null) {
             execute(line, 0);
+            System.out.println("reader 0 "+readers[0].toString());
             for (int j = 1; j < files.length; j++) {
+                System.out.println("reader 1 "+readers[j].toString());
                 line = readers[j].readLine();
                 execute(line, j);
             }
+            counter ++;
+            if(counter == 15)
+                return;
         }
 
     }
@@ -59,12 +67,12 @@ public class CacheSimulation {
                 System.out.println("fectching ... ");
                 break;
             case 2:
-                System.out.println("reading "+info[1]);
-//                processors.get(index).read(info[1]);
+//                System.out.println("reading "+info[1]);
+                processors.get(index).read(info[1]);
                 break;
             case 3:
-//                processors.get(index).write(info[1]);
-                System.out.println("writing "+info[1]);
+                processors.get(index).write(info[1]);
+//                System.out.println("writing "+info[1]);
                 break;
             default:
                 break;
@@ -75,8 +83,8 @@ public class CacheSimulation {
         try {
 //            CacheSimulation cs = new CacheSimulation(args[0], Integer.parseInt(args[2]), Integer.parseInt(args[3]), Integer.parseInt(args[4]), Integer.parseInt(args[5]));
 //            cs.run(args[1], args[2]);
-            CacheSimulation cs1 = new CacheSimulation();
-            cs1.run("fft","1");
+            CacheSimulation cs1 = new CacheSimulation("msi",2,1,1,16);
+            cs1.run("fft","2");
         } catch (Exception e) {
             e.printStackTrace();
         }
