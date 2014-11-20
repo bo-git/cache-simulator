21f5c6bd84793596569508683b1022dba6a28825
Steve Liu <steveliu.api@gmail.com>
2014-11-20 17:34:55 +0800

    moesi added to the family

---
+++ src/Protocol/MOESI.java
@@ -0,0 +1,11 @@
+package Protocol;
+
+/**
+ * Created by Bo on 11/20/14.
+ */
+public class MOESI extends MESI {
+
+    public static final int PROTOCOL = 101;
+    public static final int STATE_OWNED = 4;
+
+}
